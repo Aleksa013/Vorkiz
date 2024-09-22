9fd98959d070807670a0a25751fb229b0f089484
Aleksa013 <velma86@bk.ru>
2024-09-22 11:25:55 +0300

    feat: deploy of the templay

--- src/components/buttons.js
+++
@@ -1,6 +0,0 @@
-export const getButton = (text, parent, cb) => {
-  const button = document.createElement("button");
-  button.textContent = text;
-  button.addEventListener("click", cb);
-  parent.append(button);
-};
